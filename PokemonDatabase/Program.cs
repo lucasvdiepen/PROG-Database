e4aae775b61b42c63ab0c396b02cdddb16a1b030
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2021-11-22 14:36:04 +0100

    Setup project

---
+++ PokemonDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokemonDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
